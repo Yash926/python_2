0760fc2b95e90e96c797dc92ed6a93a1944dddd0
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-09 11:35:27 +0100

    marks and grade file

---
+++ elif.py
@@ -0,0 +1,8 @@
+marks = int(input("enter your marks:"))
+if marks > 90 and marks <= 100:
+    print("your grade is A")
+elif marks > 80 and marks <=90:
+    print("your grade is B")
+elif marks>70 and marks <=80:
+    print("Your grade is c")
+else:print("not a valid number")    
