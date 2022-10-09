653e69f43e6a3a73dcca1e0234946d97ffb17995
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-09 11:32:42 +0100

    greatest number file

---
+++ greatestNumber.py
@@ -0,0 +1,10 @@
+# Take two inputs from user(int/float)
+#print the greatest number
+num1=float(input(" Enter first number:"))
+num2=float(input("enter second number:"))
+if num1>num2:
+    print("first number is greatest")
+elif num2>num1:
+    print("second number is greatest") 
+else:
+    print("both are equal")       
