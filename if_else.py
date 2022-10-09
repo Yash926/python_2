ad03e437cfc17be0f3923851d7690fc0f11a883c
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-09 12:21:35 +0100

    if else file

---
+++ if_else.py
@@ -0,0 +1,17 @@
+#if and else
+a=5
+b=20
+if a > b:
+    print("B is greater than A")
+else:
+    print("we are in else block")
+#questiom
+x=int(input("enter your first number:"))
+y=int(input("enter your second number:"))    
+if x>y:
+    print("YAYY")
+else:
+   print("NAYY")    
+
+
+
