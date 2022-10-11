0d2a01cec523d163c451950cf4cf401664491247
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-11 18:57:21 +0100

    S.I file

---
+++ simpleinterest.py
@@ -0,0 +1,8 @@
+#program to find simple interest for
+#given principal amount,time and
+#rate of innterest
+p=float(input("Enter principal amount here:"))
+r=float(input("enter rate of interest here:"))
+t=float(input("enter time here:"))
+Si=(p*r*t)/100
+print("the simple interest for the given P.A is",Si,"Rs")
