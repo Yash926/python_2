94a6fedb5bde917c4c9173084af1880a04417d74
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-30 07:19:55 +0000

    python project

---
+++ class_python_project.py
@@ -0,0 +1,29 @@
+import random
+user_input = input("Enter a Choice : [R] - Rock, [P] - Paper, [S] - Scissor : ")
+possibleAction = ["rock", "paper", "scissor"]
+computerAction = random.choice(possibleAction)
+
+if user_input == "r" or "R" or "rock":
+    if computerAction == "rock" :
+        print("It's a Tie!")
+    elif computerAction == "paper" :
+        print("Ah! You just Lost to the Computer")
+    else :
+        print("Congo! You won against the Computer")
+elif user_input == "p" or "P" or "paper":
+    if computerAction == "rock" :
+        print("Congo! You won against the Computer")
+    elif computerAction == "paper" :
+        print("It's a Tie!")
+    else :
+        print("Ah! You just Lost to the Computer")
+elif user_input == "s" or "S" or "scissors":
+    if computerAction == "rock" :
+        print("Ah! You just Lost to the Computer")
+    elif computerAction == "paper" :
+        print("Congo! You won against the Computer")
+    else :
+        print("It's a Tie!")
+else:
+    print("Invalid Choice!")     
+
