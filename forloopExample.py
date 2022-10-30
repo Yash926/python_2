1f03a9125b8500de583eda56a952e7da74ea87ce
Yash Tripathi <tripathiyash143@gmail.com>
2022-10-30 16:26:24 +0000

    for loop example

---
+++ forloopExample.py
@@ -0,0 +1,14 @@
+# cars = ["nano","ford","tata","BMW","thar"]
+# for i in cars:
+    
+#      if i=="tata":
+#         continue
+#        # break
+    #  print(i)
+# for i in "hello world":
+#     print(i)
+# for i in range (0,10,):
+#for printing by the gap of 2
+for i in range(0,10,2):
+    print(i)
+
